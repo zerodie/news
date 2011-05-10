3afa375a9ac651be4cfa87fbf3689da857aaa8e2
Chia-Ling Lee <spiderman0103@gmail.com>
2011-05-10 20:58:56 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+News::Application.config.secret_token = 'f53abcff848f477d9621747c89ebbf6a8237fe626699022d858400eb32962f1a73ec728300624c534fee3734e03efd33b83eda295ad4128db1efc1f6e0db2051'
