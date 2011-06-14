c5c399de6b743939b2375b036a3f4d175478cdcd
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-14 16:17:03 +0800

    Finished bookmarklet v2 by smr.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -27,13 +27,14 @@ class MessagesController < ApplicationController
   
   #GET /messages/new
   def new
-    @message.title = params[:t]
-	@message.url = params[:u]
+    
   
     unless user_signed_in?
       redirect_to new_user_session_path
     else
       @message = Message.new
+      @message.title = params[:t]
+  	  @message.url = params[:u]
     end
   end
   
