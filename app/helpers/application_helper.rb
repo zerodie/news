0be68397ec70e162fe4d56a25c8b6c5a637a3dc5
Mayru Sun <smr301@gmail.com>
2011-06-18 00:17:18 +0800

    Finished like button finally

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,3 @@
 module ApplicationHelper
-	def url
-      base_url = "http://twhackernews.heroku.com/"
-      if @page.nil?
-        base_url
-      else
-        "#{base_url}messages/#{@page}"
-      end
-    end
+
 end
