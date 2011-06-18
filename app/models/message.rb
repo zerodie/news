e2c4e687246034a7dde2d355241e544cca044519
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-19 03:21:55 +0800

    delete 2

--- app/models/message.rb
+++ app/models/message.rb
@@ -6,13 +6,6 @@ class Message < ActiveRecord::Base
   belongs_to :user
   validates_uniqueness_of :url
   
-  def get_like_count newsid
-    url = 'http://twhackernews.heroku.com/messages/' + newsid.to_s
-    fql = 'SELECT url, share_count, like_count, comment_count, total_count FROM link_stat WHERE url="' + URI.escape(url) + '"' 
-    #puts 'http://api.facebook.com/method/fql.query?format=json&query=' + URI.escape(fql)
-    res = Net::HTTP.get_response(URI.parse('http://api.facebook.com/method/fql.query?format=json&query=' + URI.escape(fql)))
-    result = JSON.parse(res.body)
-    result[0]["like_count"]
-  end
+
   
 end
