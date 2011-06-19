51203230d6684d0ff2225b9e6d4e114fc9de62b5
Mayru Sun <smr301@gmail.com>
2011-06-19 12:56:14 +0800

    fix

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,13 @@
 class Message < ActiveRecord::Base
-  belongs_to :user
-  validates_uniqueness_of :url, :scope => :content
-  validates_presence_of :content, :unless => :has_url?
-  
-  def has_url?
-    if self.url == ""
-      false
-    else
-      true
-    end
-  end
+belongs_to :user
+validates_uniqueness_of :url, :scope => :content
+validates_presence_of :content, :unless => :has_url? #validate_presence_of :content if no url
+
+def has_url?
+if self.url == ""
+false
+else
+true
+end
+end
 end
