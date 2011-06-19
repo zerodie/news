0f0a125e48e71735d34ce4a9051cd71573561b90
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-19 13:26:05 +0800

    move likecount to MessageHelper

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,12 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 module MessagesHelper
+  def likeCountURL(url)
+    fql = 'SELECT url, share_count, like_count, comment_count, total_count FROM link_stat WHERE url="' + URI.escape(url) + '"' 
+    res = Net::HTTP.get_response(URI.parse('http://api.facebook.com/method/fql.query?format=json&query=' + URI.escape(fql)))
+    result = JSON.parse(res.body)
+    result[0]["like_count"] 
+  end
 end
