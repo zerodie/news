991ee4c49ea2c837728c147ba3ebdeca655511a8
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-19 11:52:40 +0800

    remove message validates_presence

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,11 +3,11 @@ class Message < ActiveRecord::Base
   validates_uniqueness_of :url, :scope => :content
   #validates_presence_of :content, :unless => has_url?
   
-  def has_url?
-    if self.url == ""
-      false
-    else
-      true
-  end
+  #def has_url?
+  #  if self.url == ""
+  #    false
+  #  else
+  #    true
+  #end
   
 end
