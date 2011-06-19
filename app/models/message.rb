c973c16d2f095a94038a47e8c7c189630e785a5f
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-19 11:41:36 +0800

    test has_url?

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ActiveRecord::Base
   belongs_to :user
   validates_uniqueness_of :url, :scope => :content
-  validates_presence_of :content, :unless => has_url
+  validates_presence_of :content, :unless => has_url?
   
   def has_url?
     url
