2a409a1580f092459c7577b07506c1143d588dc3
System Administrator <root@Dannys-MacBook-Pro.local>
2011-06-19 12:12:39 +0800

    add likecount

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
-module ApplicationHelper
+require 'net/http'
+require 'uri'
+require 'json'
 
+module ApplicationHelper
+  def likeCountURL(inurl)
+    url = inurl 
+    fql = 'SELECT url, share_count, like_count, comment_count, total_count FROM link_stat WHERE url="' + URI.escape(url) + '"' 
+    res = Net::HTTP.get_response(URI.parse('http://api.facebook.com/method/fql.query?format=json&query=' + URI.escape(fql)))
+    result = JSON.parse(res.body)
+    result[0]["share_count"] 
+  end
 end
