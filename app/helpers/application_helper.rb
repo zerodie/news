e43928715db69be168a23a4fd676f2ae221c093b
Mayru Sun <smr301@gmail.com>
2011-06-19 15:34:37 +0800

    test like button

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
   def title
-    
+    if @page.nil?
+	  "TW Hacker News"
+	else
+	  "#{@message.title} - TWHN"
+  end
+  
+  def url
+    base_url = "http://twhackernews.heroku.com/"
+    if @page.nil?
+	  base_url
+	else
+	  "#{base_url}messages/#{@page}"
   end
 end
