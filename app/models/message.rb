fee0e22f4555bd56fb4c414fc391af04e316fd83
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-19 13:33:58 +0800

    show

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,13 +1,14 @@
 class Message < ActiveRecord::Base
-belongs_to :user
-validates_uniqueness_of :url, :scope => :content
-validates_presence_of :content, :unless => :has_url? #validate_presence_of :content if no url
+  belongs_to :user
+  validates_uniqueness_of :url, :scope => :content
+  validates_presence_of :content, :unless => :has_url? #validate_presence_of :content if no url
 
-def has_url?
-if self.url == ""
-false
-else
-true
-end
-end
+  def has_url?
+    if self.url == ""
+      false
+    else
+      true
+    end
+  end
+  
 end
