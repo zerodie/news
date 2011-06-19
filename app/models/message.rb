4a92ef762511ec77061685588c7ff3473743f256
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-19 11:36:25 +0800

    validates_presence_of :content, :unless => has_url

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,9 @@
 class Message < ActiveRecord::Base
   belongs_to :user
   validates_uniqueness_of :url, :scope => :content
+  validates_presence_of :content, :unless => has_url
+  
+  def has_url?
+    url
+  end
 end
