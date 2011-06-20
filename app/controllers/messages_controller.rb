69f189fbc22a3f3a3670dec8413d3d3d21e924a2
Chia-Ling Lee <spiderman0103@gmail.com>
2011-06-20 11:21:15 +0800

    admin : current_user.id <= 3

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -60,7 +60,7 @@ class MessagesController < ApplicationController
   #GET /messages/1/edit
   def edit
     message = Message.find(params[:id])
-    if message.user == current_user
+    if message.user == current_user || current_user.id <= 3 # the same user or admin
       @message = message
     else
       flash[:notice] = "Must be the same user."
@@ -86,12 +86,12 @@ class MessagesController < ApplicationController
   #DELETE /messages/1.xml
   def destroy
     message = Message.find(params[:id])
-    #if message.user == current_user
+    if message.user == current_user || current_user.id <= 3
       message.destroy
-    #else
-      #flash[:notice] = "Must be the same user."
-      #redirect_to message
-    #end
+    else
+      flash[:notice] = "Must be the same user."
+      redirect_to message
+    end
     respond_to do |format|
       format.html { redirect_to(messages_url) }
       format.xml  { head :ok }
